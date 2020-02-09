474c9b7ea3a4840c722e49aec943f44904d66fa4
Gustave Habimana <ghabiman@gmail.com>
2020-02-09 23:17:38 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 2
 second_number = 5
-sum = (first_number + second_number)
-sum = (first_number - second_number)
-sum = (first_number * second_number)
+sum_add = (first_number + second_number)
+sum_minus = (first_number - second_number)
+sum_multiply = (first_number * second_number)
